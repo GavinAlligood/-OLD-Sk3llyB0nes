45ff639b715ca6242b03b940f46f42ca92d7aebd
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2020-08-12 11:43:32 -0400

    Updated Sk3llyB0nes.py, added port scanner
    Port scanner is still not finished, i will add other modes like SYN ACK, thorough scans, UDP, etc.

--- Sk3llyB0nes.py
+++ Sk3llyB0nes.py
@@ -20,6 +20,8 @@ print("                      |___/ ")
 
 ### simply raising the ammount of bytes sent changes things... mess with this and monitor network resources being used 
 
+# handle broken pipe exception
+
 ## Notification settings:
 ICON_PATH = "/image/skelly.png"
 notify2.init("Sk3lly B0nes")
@@ -33,13 +35,9 @@ def gather_bones():
 		port = port
 		s = socket.socket()
 	except socket.error as msg:
-		print(Style.BRIGHT)
-		print(Fore.RED + "[☠] Socket creation error: " + str(msg))
-		print(Style.RESET_ALL)
+		print(Style.BRIGHT + Fore.RED + "[☠] Socket creation error: " + str(msg) + Style.RESET_ALL)
 	except NameError:
-		print(Style.BRIGHT)
-		print(Fore.RED + "[☠] Port not defined!")
-		print(Style.RESET_ALL)
+		print(Style.BRIGHT + Fore.RED + "[☠] Port not defined!" + Style.RESET_ALL)
 		main()
 
 
@@ -50,75 +48,69 @@ def connect_bones():
 		global port
 		global s
 		s = socket.socket()
-		print(Style.BRIGHT)
-		print(Fore.BLUE + "[i] Binding socket to port: " + str(port))
-		print(Style.RESET_ALL)
+		print(Style.BRIGHT + Fore.BLUE + "[i] Binding socket to port: " + str(port) + Style.RESET_ALL)
 		s.bind((host, port))
 		s.listen(5) # number of bad connections
 	except socket.error as msg:
-		print(Style.BRIGHT)
-		print(Fore.RED + "[☠] Socket binding error: " + str(msg) + "\n" + "Retrying...")
-		print(Style.RESET_ALL)
+		print(Style.BRIGHT + Fore.RED + "[☠] Socket binding error: " + str(msg) + "\n" + "Retrying..." + Style.RESET_ALL)
 		socket_bind()
 
 # Connect
 def create_skeleton():
 	# socket MUST be listening before it can accept
 	conn, address = s.accept()
-	print(Style.BRIGHT)
-	print(Fore.BLUE + "[i] Connected to " + address[0] + ':' + str(port))
-	print(Style.RESET_ALL)
-	print(Style.BRIGHT)
+	print(Style.BRIGHT + Fore.BLUE + "[i] Connected to " + address[0] + ':' + str(port) + Style.RESET_ALL)
 	n = notify2.Notification("Shell opened!", message = 'Connection succesfully established', icon = ICON_PATH)
 	n.set_urgency(notify2.URGENCY_CRITICAL) # low, normal, critical
 	n.show()
-	send_commands(conn)
+	command_skeleton(conn)
+	# handle broken pipe exception !!!
 	conn.close()
 
-def send_commands(conn):
+def command_skeleton(conn):
 	while True:
-		cmd = input()
-		if cmd == 'quit':
-			conn.close()
-			s.close()
-			#sys.exit()
-			main()
-
-		# system commands are stored as bytes thats why you must encode them
-		if len(str.encode(cmd)) > 0:
-			conn.send(str.encode(cmd))
-			client_response = str(conn.recv(10000), "utf-8")
-			print(client_response, end="") # end='': dont give a new line at the end of cmd
+		try:
+			cmd = input()
+			if cmd == 'quit':
+				conn.close()
+				s.close()
+				# handle ctrl + c exception so sockets actually close
+				#sys.exit()
+				main()
+			# system commands are stored as bytes thats why you must encode them
+			if len(str.encode(cmd)) > 0:
+				conn.send(str.encode(cmd))
+				client_response = str(conn.recv(10000), "utf-8")
+				print(client_response, end="") # end='': dont give a new line at the end of cmd
+		except KeyboardInterrupt:
+			print("") # Another empty line
+			print("\n" + Style.BRIGHT + Fore.BLUE + "[i] Type 'quit' to close shell" + Style.RESET_ALL)
+			continue
 
 #nmap scan module
 def scan(addr):
-	scanner = nmap.PortScanner()
-	scanner.scan(addr, '1-1000')
-	print(Style.BRIGHT)
-	print(Fore.BLUE + "[i] Host found: " + scanner[addr].hostname())
-	print(Style.RESET_ALL)
-	if scanner[addr].state() == "up":
-		print(Style.BRIGHT)
-		print("[i] Status: " + Fore.GREEN + "Up")
-		print(Style.RESET_ALL)
-	elif scanner[addr].state() == "down":
-		print(Style.BRIGHT)
-		print("[i] Status: " + Fore.RED + "Down")
-		print(Style.RESET_ALL)
-	elif scanner[addr].state() == "unkown":
-		print(Style.BRIGHT)
-		print("[i] Status: " + Fore.YELLOW + "Unkown")
-		print(Style.RESET_ALL)
-	else:
-		print(Style.BRIGHT)
-		print(Fore.RED + "[☠] An unkown error occured")
-		print(Style.RESET_ALL)
+	try:
+		scanner = nmap.PortScanner()
+		scanner.scan(addr, '1-1000')
+		print(Style.BRIGHT + Fore.BLUE + "[i] Host found: " + Fore.WHITE + scanner[addr].hostname() + Style.RESET_ALL)
+		if scanner[addr].state() == "up":
+			print(Style.BRIGHT + "[i] Status: " + Fore.GREEN + "Up" + Style.RESET_ALL)
+		elif scanner[addr].state() == "down":
+			print(Style.BRIGHT + "[i] Status: " + Fore.RED + "Down" + Style.RESET_ALL)
+		elif scanner[addr].state() == "unkown":
+			print(Style.BRIGHT + "[i] Status: " + Fore.YELLOW + "Unkown" + Style.RESET_ALL)
+		else:
+			print(Style.BRIGHT + Fore.RED + "[☠] An unkown error occured" + Style.RESET_ALL)
 
-	print(Style.BRIGHT)
-	print("[i] Open ports: \n")
-	print(scanner.scaninfo())
-	print(Style.RESET_ALL)
+		print("\n" + Style.BRIGHT + Fore.BLUE + "[i] Open ports: " + Fore.WHITE)
+		print("[i] Protocol: " + scanner[addr].all_protocols()[0])
+		for p in scanner[addr]['tcp'].keys():
+			print("[i] Open port: " + str(p))
+		print(Style.RESET_ALL)
+		
 
+	except KeyError:
+		print("\n" + Style.BRIGHT + Fore.RED + "[☠] Host not specified or invalid host!" + Style.RESET_ALL + "\n")
 
 
 def main():
@@ -142,38 +134,31 @@ def main():
 					try:
 						print(Style.BRIGHT)
 						if int(cmd[5:]) > 65353 or int(cmd[5:]) < 1:
-							print(Style.BRIGHT)
-							print(Fore.RED + "[☠] You cannot use a port below 1 or above 65353") 
-							print(Style.RESET_ALL)
+							print(Style.BRIGHT + Fore.RED + "[☠] You cannot use a port below 1 or above 65353" + Style.RESET_ALL + "\n")
 						else:
 							port = int(cmd[5:])
-							print(Style.BRIGHT)
-							print(Fore.BLUE + "[i] Port set to: " + str(port))
-							print(Style.RESET_ALL)
+							print(Style.BRIGHT + Fore.BLUE + "[i] Port set to: " + str(port) + Style.RESET_ALL + "\n")
 					except ValueError:
 						## neccessary because a number with comma not counted as base 10 decimal
-						print(Style.BRIGHT)
-						print(Fore.RED + "[☠] You cannot use a port below 1 or above 65353")
-						print(Style.RESET_ALL)
+						print(Style.BRIGHT + Fore.RED + "[☠] You cannot use a port below 1 or above 65353" + Style.RESET_ALL + "\n")
 						continue
 				elif cmd[:4].lower() == "scan":
+					# name error exception needs to be handled: no host!
 					scan(cmd[5:])
 				elif cmd.lower() == "help":
 					print("\nlisten - Start listening on specified port")
 					print("port - Specify what port to listen on, for example: port 1337")
 					print("exit - closes the application")
+					print("scan - scan a host for open ports. Example: scan 127.0.0.1")
 					print("clear/cls - clears screen (cls for windows, clear for linux)\n")
 				elif cmd.lower() == "exit":
 					sys.exit()
 
 				else:
-					print(Style.BRIGHT)
-					print(Fore.RED + "☠ Get your bones together!! ☠")
-					print(Style.RESET_ALL)
+					print("\n" + Style.BRIGHT + Fore.RED + "☠ Get your bones together!! ☠" + Style.RESET_ALL + "\n")
 		except KeyboardInterrupt:
-			print(Style.BRIGHT)
-			print("\n" + Fore.BLUE + "[i] Type exit to close program")
-			print(Style.RESET_ALL)
+			print("") # empty line for asthetic purposes
+			print("\n" + Style.BRIGHT + Fore.BLUE + "[i] Type exit to close program" + Style.RESET_ALL + "\n")
 			continue
 
 main()
